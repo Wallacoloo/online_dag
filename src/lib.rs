cf6a3da94891cde0447baed8ed5e612ccbd5ff7f
Colin Wallace <wallacoloo@gmail.com>
2016-11-04 23:49:33 -0700

    DAG checks that it's never handling nodes owned by a different DAG instance.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@ pub trait Dag<N : Eq + Hash, E : Eq + Hash + Clone> {
 
 pub struct DagNodeHandle<N, E> {
     node: Rc<RefCell<DagNode<N, E>>>,
+    /// keep a pointer to the tree owner to enforce mutability rules across multiple trees.
+    owner: *const OnDag<N, E>,
 }
 
 #[derive(PartialEq, Eq, Clone)]
@@ -32,8 +34,6 @@ pub struct DagEdge<N, E> {
 struct DagNode<N, E> {
     value: N,
     children: HashSet<DagEdge<N, E>>,
-    /// keep a pointer to the tree owner to enforce mutability rules across multiple trees.
-    owner: *const OnDag<N, E>,
 }
 
 // TODO: use a small-size optimized Set, e.g. smallset
@@ -52,10 +52,13 @@ pub struct OnDag<N, E> {
 impl <N : Eq + Hash, E : Eq + Hash + Clone> Dag<N, E> for OnDag<N, E> {
     type NodeHandle = DagNodeHandle<N, E>;
     fn add_node(&mut self, node_data: N) -> Self::NodeHandle {
-        let handle = Self::NodeHandle::new(DagNode::new(self, node_data));
+        let handle = Self::NodeHandle::new(self, DagNode::new(node_data));
         handle
     }
     fn add_edge(&mut self, from: Self::NodeHandle, to: Self::NodeHandle, data: E) -> Result<(),()> {
+        // the edge must connect two nodes owned by *this* graph.
+        assert_eq!(from.owner, self as *const Self);
+        assert_eq!(to.owner, self as *const Self);
         if self.is_reachable(&from, &to) {
             // there is a path from `to` to `from`, so adding an edge `from` -> `to` will introduce
             // a cycle.
@@ -67,6 +70,9 @@ impl <N : Eq + Hash, E : Eq + Hash + Clone> Dag<N, E> for OnDag<N, E> {
         }
     }
     fn rm_edge(&mut self, from: Self::NodeHandle, to: Self::NodeHandle, data: E) -> Result<(), ()> {
+        // the edge must belong to *this* graph.
+        assert_eq!(from.owner, self as *const Self);
+        assert_eq!(to.owner, self as *const Self);
         // delete the parent -> child relationship:
         from.node.borrow_mut().children.remove(&DagEdge::new(to.clone(), data.clone()));
         Ok(())
@@ -82,6 +88,8 @@ impl <N: Eq, E: Eq + Hash> OnDag<N, E> {
     }
     /// Compute the topological ordering of `self`.
     pub fn iter_topo(&self, from: &DagNodeHandle<N, E>) -> impl Iterator<Item=DagNodeHandle<N, E>> {
+        // can only iterate over nodes owned by *this* graph.
+        assert_eq!(from.owner, self as *const Self);
         // just a depth-first sort, but then reverse the results.
         let mut ordered = vec![];
         self.depth_first_sort(from, &mut ordered, &mut HashSet::new());
@@ -90,6 +98,8 @@ impl <N: Eq, E: Eq + Hash> OnDag<N, E> {
     }
     /// Compute the *reverse* topological ordering of `self`, i.e. children -> root
     pub fn iter_topo_rev(&self, from: &DagNodeHandle<N, E>) -> impl Iterator<Item=DagNodeHandle<N, E>> {
+        // can only iterate over nodes owned by *this* graph.
+        assert_eq!(from.owner, self as *const Self);
         // just a depth-first sort:
         // TODO: we can achieve this with lower latency by moving it into an iterator.
         let mut ordered = vec![];
@@ -118,18 +128,20 @@ impl <N : Eq + Hash, E : Eq + Hash> OnDag<N, E> {
 }
 
 impl<N : Eq + Hash, E : Eq + Hash> DagNode<N, E> {
-    fn new(owner: &OnDag<N, E>, value: N) -> Self {
+    fn new(value: N) -> Self {
         DagNode {
             value: value,
             children: HashSet::new(),
-            owner: owner as *const OnDag<N, E>,
         }
     }
 }
 
 impl<N, E> Clone for DagNodeHandle<N, E> {
     fn clone(&self) -> Self {
-        DagNodeHandle{ node: self.node.clone() }
+        DagNodeHandle {
+            node: self.node.clone(),
+            owner: self.owner,
+        }
     }
 }
 
@@ -146,8 +158,11 @@ impl<N, E> PartialEq for DagNodeHandle<N, E> {
 impl<N, E> Eq for DagNodeHandle<N, E> {}
 
 impl<N, E> DagNodeHandle<N, E> {
-    fn new(node: DagNode<N, E>) -> Self {
-        DagNodeHandle{ node: Rc::new(RefCell::new(node))}
+    fn new(owner: &OnDag<N, E>, node: DagNode<N, E>) -> Self {
+        DagNodeHandle {
+            node: Rc::new(RefCell::new(node)),
+            owner: owner,
+        }
     }
 }
 
