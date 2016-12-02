481547b21669e803483141a946a3908a2c17465e
Colin Wallace <wallacoloo@gmail.com>
2016-12-01 17:22:33 -0800

    Test for 0-cost cycles

--- src/tests/testposcostdag.rs
+++ src/tests/testposcostdag.rs
@@ -8,25 +8,23 @@ type MyDag = PosCostDag<u32, u32>;
 def_ondag_tests!{MyDag}
 
 
-/*
 #[test]
 /// Graph should not allow ZERO-COST cycles - should be an error when adding a cycle & structure should be
 /// unmodified.
 fn test_cycles() {
-    //     12
-    //     v
-    //     1
-    //     v
-    //     2
+    // A --4--> B --0--> C
+    // Adding a zero-path from C -> A should *not* introduce a 0-cycle
+    // Adding above + a zero-path from A->B *should* introduce a 0-cycle
+    // Here A, B, C become 11, 12, 13
     let mut dag = MyDag::new();
-    let root = dag.add_node(12);
-    let n2 = dag.add_node(2);
-    let n1 = dag.add_node(1);
-    dag.add_edge(&root, &n1, 1001).expect("Failed to add edge");
-    dag.add_edge(&n1, &n2, 1002).expect("Failed to add edge");
-    dag.add_edge(&n2, &root, 1003).err().expect("Failed to detect cycle");
+    let a = dag.add_node(11);
+    let b = dag.add_node(12);
+    let c = dag.add_node(13);
+    dag.add_edge(&a, &b, 4).expect("Failed to add edge");
+    dag.add_edge(&b, &c, 0).expect("Failed to add edge");
+    dag.add_edge(&c, &a, 0).expect("Failed to add edge");
+    dag.add_edge(&a, &b, 0).err().expect("Failed to detect cycle");
 }
-*/
 
 impl CostQueriable<MyDag> for u32 {
     /// For testing, the edge cost is identical to its weight.
