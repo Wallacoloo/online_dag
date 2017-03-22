117907c15d9f57ff5bbf329211ffc5e269506eef
Colin Wallace <wallacoloo@gmail.com>
2017-03-21 23:44:11 -0700

    Use a HashSet for storing edges instead of a Vec.
    We need the insert/remove by value, which Vec doesn't offer.

--- src/iodag.rs
+++ src/iodag.rs
@@ -6,55 +6,67 @@
 /// Each Edge type allows a different type of weight.
 ///
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
 /// W=Weight
-pub struct IODag<N, FromNodeW, FromNullW, ToNodeW, ToNullW> {
+pub struct IODag<N, FromNodeW, FromNullW, ToNodeW, ToNullW>
+    where FromNodeW: Hash + Eq + PartialEq, FromNullW: Hash + Eq + PartialEq, ToNodeW: Hash + Eq + PartialEq, ToNullW: Hash + Eq + PartialEq {
     /// To create unique NodeHandles, we just assign them unique u64's from this counter.
     node_counter: u64,
     /// Node states, including their outgoing edges.
     node_data: HashMap<NodeHandle, NodeData<N, FromNodeW, FromNullW, ToNodeW, ToNullW>>,
     /// Edges that start at null.
-    edges_from_null: Vec<Edge<FromNodeW, FromNullW, ToNodeW, ToNullW>>,
+    edges_from_null: HashSet<Edge<FromNodeW, FromNullW, ToNodeW, ToNullW>>,
 }
 
-pub struct NodeData<N, FromNodeW, FromNullW, ToNodeW, ToNullW> {
+pub struct NodeData<N, FromNodeW, FromNullW, ToNodeW, ToNullW>
+    where FromNodeW: Hash + Eq + PartialEq, FromNullW: Hash + Eq + PartialEq, ToNodeW: Hash + Eq + PartialEq, ToNullW: Hash + Eq + PartialEq {
     /// userdata associated with this node
     data: N,
     /// Edges leaving this node
-    outbound: Vec<Edge<FromNodeW, FromNullW, ToNodeW, ToNullW>>,
+    outbound: HashSet<Edge<FromNodeW, FromNullW, ToNodeW, ToNullW>>,
 }
 
 // Edges consist of two parts: the `from` and the `to`.
 // Define this structure in a way that creates 4 type of edges.
+#[derive(Eq, Hash, PartialEq)]
 pub struct FromNull<W> {
     weight: W,
 }
+#[derive(Eq, Hash, PartialEq)]
 pub struct FromNode<W> {
     node: NodeHandle,
     weight: W,
 }
 
+#[derive(Eq, Hash, PartialEq)]
 pub struct ToNull<W> {
     weight: W,
 }
+#[derive(Eq, Hash, PartialEq)]
 pub struct ToNode<W> {
     node: NodeHandle,
     weight: W,
 }
 
 
-pub enum EdgeFrom<FromNodeW, FromNullW> {
+#[derive(Eq, Hash, PartialEq)]
+pub enum EdgeFrom<FromNodeW, FromNullW>
+    where FromNodeW: Hash + Eq + PartialEq, FromNullW: Hash + Eq + PartialEq {
     Null(FromNull<FromNullW>),
     Node(FromNode<FromNodeW>),
 }
-pub enum EdgeTo<ToNodeW, ToNullW> {
+#[derive(Eq, Hash, PartialEq)]
+pub enum EdgeTo<ToNodeW, ToNullW>
+    where ToNodeW: Hash + Eq + PartialEq, ToNullW: Hash + Eq + PartialEq {
     Null(ToNull<ToNullW>),
     Node(ToNode<ToNodeW>),
 }
 
-pub struct Edge<FromNodeW, FromNullW, ToNodeW, ToNullW> {
+#[derive(Eq, Hash, PartialEq)]
+pub struct Edge<FromNodeW, FromNullW, ToNodeW, ToNullW>
+    where FromNodeW: Hash + Eq + PartialEq, FromNullW: Hash + Eq + PartialEq, ToNodeW: Hash + Eq + PartialEq, ToNullW: Hash + Eq + PartialEq {
     from: EdgeFrom<FromNodeW, FromNullW>,
     to: EdgeTo<ToNodeW, ToNullW>,
 }
@@ -64,12 +76,13 @@ pub struct NodeHandle {
     index: u64,
 }
 
-impl<N, FromNodeW, FromNullW, ToNodeW, ToNullW> IODag<N, FromNodeW, FromNullW, ToNodeW, ToNullW> {
+impl<N, FromNodeW, FromNullW, ToNodeW, ToNullW> IODag<N, FromNodeW, FromNullW, ToNodeW, ToNullW>
+    where FromNodeW: Hash + Eq + PartialEq, FromNullW: Hash + Eq + PartialEq, ToNodeW: Hash + Eq + PartialEq, ToNullW: Hash + Eq + PartialEq {
     pub fn new() -> Self {
         IODag {
             node_counter: 0,
             node_data : HashMap::new(),
-            edges_from_null: Vec::new(),
+            edges_from_null: HashSet::new(),
         }
     }
     pub fn new_node(&mut self, node_data: N) -> NodeHandle {
