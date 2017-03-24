a3c0d2ac7327965358d5221806857dbf43da6d8a
Colin Wallace <wallacoloo@gmail.com>
2017-03-24 00:55:44 -0700

    Store both incoming and outgoing edges at each node, for fast reverse-traversal

--- src/iodag.rs
+++ src/iodag.rs
@@ -11,14 +11,19 @@ pub struct IODag<N, W>
     where W: Hash + Eq + PartialEq {
     /// To create unique NodeHandles, we just assign them unique u64's from this counter.
     node_counter: u64,
-    outbound_edges: HashMap<Option<NodeHandle>, HashSet<Edge<W>>>,
+    edges: HashMap<Option<NodeHandle>, EdgeSet<W>>,
     node_data: HashMap<NodeHandle, N>,
 }
 
-
-#[derive(Eq, Hash, PartialEq)]
-pub struct Edge<W>
+/// Include both the outbound and inbound edges associated with a Node.
+struct EdgeSet<W>
     where W: Hash + Eq + PartialEq {
+    outbound: HashSet<Edge<W>>,
+    inbound: HashSet<Edge<W>>,
+}
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+pub struct Edge<W> {
     from: Option<NodeHandle>,
     to: Option<NodeHandle>,
     weight: W,
@@ -31,15 +36,24 @@ pub struct NodeHandle {
 }
 
 impl<N, W> IODag<N, W>
-    where W: Hash + Eq + PartialEq { 
+    where W: Clone + Hash + Eq + PartialEq {
     pub fn new() -> Self {
-        let mut outbound_edges = HashMap::new();
-        outbound_edges.insert(None, HashSet::new());
+        let mut edges = HashMap::new();
+        edges.insert(None, EdgeSet::new());
         IODag{
             node_counter: 0,
-            outbound_edges: outbound_edges,
+            edges: edges,
             node_data : HashMap::new(),
         }
+    }
+    pub fn node_data(&self, node: NodeHandle) -> &N {
+        &self.node_data[&node]
+    }
+    pub fn iter_outbound_edges<'a>(&'a self, node: Option<NodeHandle>) -> impl Iterator<Item=&Edge<W>> + 'a {
+        self.edges[&node].outbound.iter()
+    }
+    pub fn iter_inbound_edges<'a>(&'a self, node: Option<NodeHandle>) -> impl Iterator<Item=&Edge<W>> + 'a {
+        self.edges[&node].inbound.iter()
     }
     pub fn add_node(&mut self, node_data: N) -> NodeHandle {
         let handle = NodeHandle {
@@ -48,7 +62,7 @@ impl<N, W> IODag<N, W>
         self.node_counter = self.node_counter+1;
         // Create storage for the node's outgoing edges
         // Panic if the NodeHandle was somehow already in use.
-        assert!(self.outbound_edges.insert(Some(handle), HashSet::new()).is_none());
+        assert!(self.edges.insert(Some(handle), EdgeSet::new()).is_none());
         // Store the node's data
         assert!(self.node_data.insert(handle, node_data).is_none());
         handle
@@ -66,24 +80,26 @@ impl<N, W> IODag<N, W>
         };
 
         if safe_to_add {
-            self.outbound_edges.get_mut(&edge.from).unwrap().insert(edge);
+            self.edges.get_mut(&edge.from).unwrap().outbound.insert(edge.clone());
+            self.edges.get_mut(&edge.to).unwrap().inbound.insert(edge);
             Ok(())
         } else {
             Err(())
         }
     }
     /// Removes the edge (if it exists).
-    /// Returns true if the edge was previously present
-    pub fn del_edge(&mut self, edge: Edge<W>) -> bool {
-        match self.outbound_edges.get_mut(&edge.from) {
-            None => false,
-            Some(edge_set) => edge_set.remove(&edge),
+    pub fn del_edge(&mut self, edge: Edge<W>) {
+        if let Some(edge_set) = self.edges.get_mut(&edge.from) {
+            edge_set.outbound.remove(&edge);
+        }
+        if let Some(edge_set) = self.edges.get_mut(&edge.to) {
+            edge_set.inbound.remove(&edge);
         }
     }
 
     /// Return true if and only if `search` is reachable from (or is equal to) `base`
     fn is_reachable(&self, search: NodeHandle, base: NodeHandle) -> bool {
-        (base == search) || self.outbound_edges[&Some(base)].iter().any(|edge| {
+        (base == search) || self.edges[&Some(base)].outbound.iter().any(|edge| {
             match edge.to {
                 // Edge to Null
                 None => false,
@@ -93,4 +109,12 @@ impl<N, W> IODag<N, W>
     }
 }
 
-
+impl<W> EdgeSet<W>
+    where W: Hash + Eq + PartialEq {
+    fn new() -> Self {
+        EdgeSet {
+            outbound: HashSet::new(),
+            inbound: HashSet::new(),
+        }
+    }
+}
